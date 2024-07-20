e9a66c8a2ee4664d674cd3970703a4c4b82bfb9c
nilocnt <nilocnt@gmail.com>
2024-07-20 15:14:00 -0300

    Add files via upload

---
+++ changelog.md
@@ -0,0 +1,11 @@
+## Change Log:
+- 1.0   
+
+    * Vulkan Optimization Turnip
+
+Feature :
+
+-Freeadreno Turnip weekly release!
+-change renderer to vulkan
+-disable debug logging
+-disable gpu trace
---
+++ install.sh
@@ -0,0 +1,18 @@
+SKIPMOUNT=false
+PROPFILE=true
+POSTFSDATA=true
+LATESTARTSERVICE=true
+
+REPLACE="
+"
+sleep 2
+ui_print " • Module info "
+sleep 0.5
+ui_print " • vulkan optimization turnip"
+sleep 0.5
+ui_print " • stable Release "
+sleep 0.5
+ui_print " • @nilocnt"
+sleep 0.5
+ui_print " • Finish"
+sleep 1
